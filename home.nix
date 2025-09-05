0555abf3216f78145a399e0282f9d320691e7a00
sudhanshunitinatalkar <atalkarsudhanshu@oroton.me>
2025-09-06 01:54:05 +0530

    config update: 2025-09-06 01:54:05

--- home.nix
+++ home.nix
@@ -33,10 +33,10 @@
            nrs()
            {
                 cd ~/nixos-cosmos-config || return
-                git pull --rebase origin main
+                git pull --rebase origin main || echo "Pull failed, only commiting changes"
                 git add .
                 git commit -m "config update: $(date "+%Y-%m-%d %H:%M:%S")"
-                git push origin main
+                git push origin main || echo "Push failed, internet down?"
                 sudo nixos-rebuild switch --flake .#cosmos 
             }  
         '';
